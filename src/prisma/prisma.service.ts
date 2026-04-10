8a82b076726e98888d5eea4cc05d2c33d1ecc2e2
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-04-10 23:01:42 +0400

    feat: creating global prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+import { Pool} from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL || "file:./dev.db";
+
+if(!connectionString){
+    throw new Error("DATABASE_URL environment variable is not set");
+}
+
+const pool = new Pool((connectionString));
+const adapter = new PrismaPg(pool);
+
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor(){
+        super({adapter});
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+        await pool.end();
+    }
+}
