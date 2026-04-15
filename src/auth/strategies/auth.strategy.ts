3c1d96a0f6e0935847179bb28aad1fa281a379ef
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-04-15 11:49:10 +0400

    refactor: changing stratregy to hybrid extraction

--- src/auth/strategies/auth.strategy.ts
+++ src/auth/strategies/auth.strategy.ts
@@ -1,12 +1,26 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy, ExtractJwt} from "passport-jwt";
+import { Request } from "express";
+import { Injectable } from "@nestjs/common";
 
-export class AuthStrategy extends PassportStrategy(Strategy) {
+@Injectable()
+export class AuthStrategy extends PassportStrategy(Strategy, 'jwt') {
     constructor(){
         super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), 
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                
+                ExtractJwt.fromAuthHeaderAsBearerToken(),
+
+            (req: Request) => {
+                let token = null;
+                if(req && req.cookies){
+                    token = req.cookies['access_token']
+                }
+               return token; 
+            }
+        ]),
             ignoreExpiration: false,
-            secretOrKey: process.env.JWT_SECRET
+            secretOrKey: process.env.JWT_SECRET || 'default_secret_key'
         });
     }
 
