ec62587e664ae1b487611bceb865be0662802223
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-04-21 16:43:54 +0400

    feat(cloudinary): instlling cloudinary and crating main boilerplate setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { AddressesModule } from './addresses/addresses.module';
 import { CategoriesModule } from './categories/categories.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 
 @Module({
-  imports: [AuthModule, AddressesModule, CategoriesModule],
+  imports: [AuthModule, AddressesModule, CategoriesModule, CloudinaryModule],
   controllers: [],
   providers: [],
 })
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -3,7 +3,7 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class CategoriesService {
-    constructor (private readonly prosmaService: PrismaService){}
+    constructor (private readonly prisma: PrismaService){}
 
     async createCategory(){}
 }
