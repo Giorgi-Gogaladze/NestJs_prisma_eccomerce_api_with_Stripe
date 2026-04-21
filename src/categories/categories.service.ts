e37123facf0c834b6ad7ae012a5dd7d3e3aa87d5
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-04-21 17:41:16 +0400

    feat(category): adding category update funciton

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,8 +1,10 @@
-import { Body, ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateCategoryDto } from './dtos/create_category.dto';
 import slugify from 'slugify'
 import { CloudinaryService } from '../cloudinary/cloudinary.service';
+import { Category } from '@prisma/client';
+import { UpdateCategoryDto } from './dtos/update_category.dto';
 
 @Injectable()
 export class CategoriesService {
@@ -11,7 +13,8 @@ export class CategoriesService {
         private readonly cloudinaryService: CloudinaryService
     ){}
 
-    async createCategory(dto: CreateCategoryDto, file: Express.Multer.File){
+
+    async createCategory(dto: CreateCategoryDto, file: Express.Multer.File): Promise<Category>{
         const existingCategory = await this.prisma.category.findFirst({
             where: {
                     name: dto.name,
@@ -26,11 +29,12 @@ export class CategoriesService {
         const categorySlug = slugify(dto.name, {
             lower: true,
             strict: true,
-            replacement: '_'
+            replacement: '-'
         });
 
         let thumbnailUrl = null;
         if(file){
+            //ქლაუდინარიზე ატვირთვა
             const uplaodRes = await this.cloudinaryService.UploadFile(file);
             thumbnailUrl = uplaodRes.secure_url;
         }
@@ -46,6 +50,36 @@ export class CategoriesService {
             }
         })
 
+    }
+
+
+    async updateCategory(id: string, dto: UpdateCategoryDto, file?: Express.Multer.File): Promise<Category>{
+        const exists = await this.prisma.category.findUnique({
+            where: { id }
+        });
+        if(!exists){
+            throw new NotFoundException(`Category with id: ${id} not found`);
+        }
+        const updateData: any = { ...dto};
+        
+        if(dto.name){
+            updateData.slug = slugify(dto.name, 
+            {
+                lower: true,
+                strict: true,
+                replacement: '-'
+            });
+        }
+
+        if(file){
+            const uploadRes = await this.cloudinaryService.UploadFile(file);
+            updateData.thumbnailUrl = uploadRes.secure_url;
+        }
+
+        return await this.prisma.category.update({
+            where: {id},
+            data: updateData
+        })
     }
 
 }
