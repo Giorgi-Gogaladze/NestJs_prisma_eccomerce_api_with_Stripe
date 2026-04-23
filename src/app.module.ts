726801ca87ce1ae445f5a4d6fb2d30f980226906
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-04-23 20:25:36 +0400

    feat(redis): configure cache manager with redis store and environment variable support

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,28 @@ import { CategoriesModule } from './categories/categories.module';
 import { CloudinaryModule } from "./cloudinary/cloudinary.module";
 import { CouponsModule } from './coupons/coupons.module';
 import { CacheModule } from '@nestjs/cache-manager'
-import KeyvRedis from "@keyv/redis";
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { redisStore } from 'cache-manager-redis-yet'
 
 
 @Module({
   imports: [
-    CacheModule.register({
-      isGlobal: true,
-      stores: [
-        new KeyvRedis('redis://localhost:6379'),
-      ],
-      ttl: 600000
-    }),
-    AuthModule, AddressesModule, CategoriesModule, CloudinaryModule, CouponsModule],
+  ConfigModule.forRoot({isGlobal: true}), //.env_სთვის()
+  CacheModule.registerAsync({
+    isGlobal: true,
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => {
+      const store = await redisStore({
+        url: configService.get('REDIS_URL'),  //.env_დან
+        ttl: 600000
+      });
+      return {
+        store: store as any
+      }
+    },
+    inject: [ConfigService]
+  }), 
+  AuthModule, AddressesModule, CategoriesModule, CloudinaryModule, CouponsModule],
   controllers: [],
   providers: [],
 })
