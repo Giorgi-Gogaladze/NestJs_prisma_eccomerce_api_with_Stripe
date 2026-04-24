4d4bbab7bc845441feb463f1b80ed7a254658abd
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-04-24 14:47:55 +0400

    feat(attributes): implement CRUD operations for attributes with guards and DTOs

--- src/attributes/attributes.controller.ts
+++ src/attributes/attributes.controller.ts
@@ -1,7 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { AttributesService } from './attributes.service';
+import { CreateAttributeDto } from './dots/create_attribute.dto';
+import { Attribute } from '@prisma/client';
+import { UpdateAttributeDto } from './dots/update_attribute.dto';
+import { AtGuard } from '../guards/at.guard';
+import { RolesGuard } from '../guards/roles.guard';
+import { Roles } from '../custom_decorators/roles.decorator';
+import { Public } from '../custom_decorators/public.decorator';
 
+@UseGuards(AtGuard, RolesGuard)
+@Roles('ADMIN')
 @Controller('attributes')
 export class AttributesController {
   constructor(private readonly attributesService: AttributesService) {}
+
+
+  @Post()
+  async createAttribute(
+    @Body() dto: CreateAttributeDto
+  ): Promise<Attribute>{
+    return await this.attributesService.createAttribute(dto);
+  }
+
+  @Public()
+  @Get()
+  async getAttributes(): Promise<Attribute[] | []>{
+    return await this.attributesService.getAttributes();
+  }
+
+
+  @Patch(':id')
+  async updateAttribute(
+    @Param('id') id: string,
+    @Body() dto: UpdateAttributeDto
+  ): Promise<Attribute>{
+    return await this.attributesService.updateAttribute(id, dto);
+  }
+
+
+  @Delete(":id")
+  async deleteAttribute(
+    @Param('id') id: string
+  ): Promise<{message: string}>{
+    return await this.attributesService.deleteAttribute(id);
+  }
 }
