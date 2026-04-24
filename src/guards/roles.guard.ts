89ab0b59440a5be75450ddb809e8fdbcd50c157c
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-04-24 14:50:58 +0400

    feat(guards): enhance RolesGuard to include public access checks

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -1,18 +1,37 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
+import { IS_PUBLIC_KEY } from "../custom_decorators/public.decorator";
 
 @Injectable()
 export class RolesGuard implements CanActivate{
     constructor( private reflector: Reflector){}
 
     canActivate(context : ExecutionContext): boolean {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [  //(ჩემთვის) ვამოწმებ არის თუ არა ფაბლიქი, პრველ რიგში ეს მოწმდება
+            context.getHandler(),
+            context.getClass(),
+        ]);
+
+        if (isPublic) { //ტუ არის, ნებას ვრთავ წვდომაზე
+            return true; 
+        }
+
+
         const requiredRoles = this.reflector.getAllAndOverride<string[]>('roles', [
             context.getHandler(),
             context.getClass(),
         ])
 
-        if(!requiredRoles) return true;
-        const {user} = context.switchToHttp().getRequest();
+
+        if(!requiredRoles || requiredRoles.length === 0){
+            return true;
+        }
+
+        const{ user } = context.switchToHttp().getRequest();
+
+        if(!user){
+            return false;
+        }
 
         return requiredRoles.some((role) => user.role?.includes(role));
     }
