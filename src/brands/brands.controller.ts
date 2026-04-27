a81579db7f7a5fddb318dd507dd6ede6790dca69
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-04-27 11:02:26 +0400

    feat(brands): add guards and decorators for role-based access control in BrandsController

--- src/brands/brands.controller.ts
+++ src/brands/brands.controller.ts
@@ -1,14 +1,20 @@
-import { Body, Controller, Delete, Get, HttpStatus, Param, ParseFilePipeBuilder, Patch, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, ParseFilePipeBuilder, Patch, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { BrandsService } from './brands.service';
 import { CreateBrandDto } from './dtos/create_brand.dto';
 import { Brand } from '@prisma/client';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UpdateBrandDto } from './dtos/update_brand.dto';
+import { AtGuard } from '../guards/at.guard';
+import { RolesGuard } from '../guards/roles.guard';
+import { Roles } from '../custom_decorators/roles.decorator';
+import { Public } from '../custom_decorators/public.decorator';
 
+@UseGuards(AtGuard, RolesGuard)
 @Controller('brands')
 export class BrandsController {
   constructor(private readonly brandsService: BrandsService) {}
 
+  @Roles('ADMIN')
   @Post()
   @UseInterceptors(FileInterceptor('logoImg'))
   async createBrand( 
@@ -30,13 +36,14 @@ export class BrandsController {
     return await this.brandsService.createBrand(dto, logoImg);
   }
 
-
+  @Public()
   @Get()
   async getBrands(): Promise<Brand[] | []>{
     return await this.brandsService.getBrands();
   }
 
 
+  @Roles('ADMIN')
   @Patch(':id')
   @UseInterceptors(FileInterceptor('logoImg'))
   async updateBrand(
@@ -60,7 +67,7 @@ export class BrandsController {
     return await this.brandsService.updateBrand(id, dto, logoImg);
   }
 
-
+  @Roles('ADMIN')
   @Delete(':id')
   async deleteBrand(
     @Param('id') id: string
