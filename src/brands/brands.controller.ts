6891b91a873cdad4f5736f0eed114eb2758031e8
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-04-27 09:51:04 +0400

    feat(brands): rename logoUrl to logoImg in Brand model and update createBrand method

--- src/brands/brands.controller.ts
+++ src/brands/brands.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { BrandsService } from './brands.service';
+import { CreateBrandDto } from './dtos/create_brand.dto';
+import { Brand } from '@prisma/client';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('brands')
 export class BrandsController {
   constructor(private readonly brandsService: BrandsService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('logoImg'))
+  async createBrand( 
+    @Body() dto: CreateBrandDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+      .addFileTypeValidator({
+        fileType: /(jpg|jpeg|png|webp)$/
+      })
+      .addMaxSizeValidator({
+        maxSize: 2 * 1024 * 1024
+      })
+      .build({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
+      }),
+    )
+    logoImg: Express.Multer.File
+  ): Promise<Brand>{
+    return await this.brandsService.createBrand(dto, logoImg);
+  }
+
+
+
+  
 }
