4e944df3df10af19c9a7235b600817861ff028a1
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-05-05 13:52:32 +0400

    feat(prodVariant): creating pirvate  funciton for  sku generation and enhancing  product creation function

--- src/product_variants/product_variants.service.ts
+++ src/product_variants/product_variants.service.ts
@@ -1,18 +1,41 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { createProductVariantDto } from './dtos/create_product_variant.dto';
+import { Product } from '@prisma/client';
 
 @Injectable()
 export class ProductVariantsService {
     constructor(private readonly prisma: PrismaService){}
 
-    async createProductVariant(productId: string, dto: createProductVariantDto){
-        const ExistingVariants = await this.prisma.productVariant.findMany({
-            where: { productId},
-            include: { attribute_values: {select: {id: true}}} 
-        });
 
-        const isDuplicate = ExistingVariants.some((variant) => {
+
+    private generateSku(values: string[],  productName: string): string{
+        const name = productName.slice(0, 3).toUpperCase();
+        const valueParts = values.map((val) => {
+            val.replace(/\s+/g, '').slice(0, 5).toUpperCase()
+        }).join('-');
+
+        const randomSufix = Math.floor(1000 + Math.random() * 9000);
+
+        return `${name}-${valueParts}-${randomSufix}`;
+    }
+
+
+
+    async createProductVariant(productId: string, dto: createProductVariantDto){
+        const product = await this.prisma.product.findUnique({
+            where: { id: productId},
+            include: {
+                product_variants: {
+                    include: {attribute_values: {
+                        select: {id: true}
+                    }}
+                }
+             } 
+        });
+        if(!product) throw new ConflictException('Product not found');
+
+        const isDuplicate = product.product_variants.some((variant) => {
             const existingValuesId =variant.attribute_values.map(v => v.id);
 
             return (
@@ -21,11 +44,19 @@ export class ProductVariantsService {
             )
         });
 
+        const selectedValues = await this.prisma.attributeValue.findMany({
+            where: {id: { in: dto.attributeValueIds}} 
+        });
+        const valNames = selectedValues.map(v => v.value);
+
+        const sku  = this.generateSku(valNames, product.name)
+
+
         if(isDuplicate) throw new ConflictException('A variant with these attribtue values already exist');
 
         return await this.prisma.productVariant.create({
             data: {
-                sku: 'hello',
+                sku: sku,
                 price: dto.price,
                 stock: dto.stock,
                 productId,
@@ -34,6 +65,7 @@ export class ProductVariantsService {
                 }
             }
         })
-    }
+    };
+
 
 }
