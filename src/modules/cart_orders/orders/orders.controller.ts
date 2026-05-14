b7586e46d93f6892514cb69fb0894ce8baea0778
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-05-14 17:53:57 +0400

    feat(order): adding AT and roles guards to the controller

--- src/modules/cart_orders/orders/orders.controller.ts
+++ src/modules/cart_orders/orders/orders.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { DetailedOrder, OrdersService, OrderWithCount, OrderWithItems } from './orders.service';
 import { User } from '../../../shared/custom_decorators/user.decorator';
 import { createOrderDto } from './dtos/create_order.dto';
 import { UpdateOrderStatusDto } from './dtos/update_order_status.dto';
 import { Order } from '@prisma/client';
+import { AtGuard } from '../../../shared/guards/at.guard';
+import { RolesGuard } from '../../../shared/guards/roles.guard';
+import { Roles } from '../../../shared/custom_decorators/roles.decorator';
 
+@UseGuards(AtGuard, RolesGuard)
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
@@ -24,15 +28,18 @@ export class OrdersController {
     return await this.ordersService.getMyOrders(user.sub)
   }
 
+  @Roles('ADMIN')
   @Get()
   async getActiveOrders(): Promise<Order[]>{
     return await this.ordersService.getActiveOrders()
   }
 
+  @Roles('ADMIN')
   @Get()
   async getCanceledOrders(){
     return await this.ordersService.getCanceledOrders()
   }
+
 
   @Get(':orderId')
     async getOrderById(
@@ -42,6 +49,7 @@ export class OrdersController {
       return await this.ordersService.getOrderById(orderId, user.sub)
   }
 
+  @Roles('ADMIN')
   @Patch('/:orderId')
   async updateStatus(
     @Param('orderId') orderId: string,
@@ -50,6 +58,7 @@ export class OrdersController {
     return await this.ordersService.updateStatus(orderId, dto)
   }
 
+  
   @Patch('cancel/:orderId')
   async cancelOrder(
     @Param('orderId') orderId: string,
